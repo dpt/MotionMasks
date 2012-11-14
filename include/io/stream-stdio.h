6fb8ab0c331447c0d6adfdd5546bd7b6017e4b47
David Thomas <dave@davespace.co.uk>
2012-11-14 21:42:55 +0000

    Stream libraries.

---
+++ include/io/stream-stdio.h
@@ -0,0 +1,19 @@
+/* --------------------------------------------------------------------------
+ *    Name: stream-stdio.h
+ * Purpose: C standard IO stream implementation
+ * ----------------------------------------------------------------------- */
+
+#ifndef STREAM_STDIO_H
+#define STREAM_STDIO_H
+
+#include <stdio.h>
+
+#include "base/error.h"
+
+#include "io/stream.h"
+
+/* use 0 for a sensible default buffer size */
+
+mmerror_t stream_stdio_create(FILE *f, int bufsz, stream_t **s);
+
+#endif /* STREAM_STDIO_H */
