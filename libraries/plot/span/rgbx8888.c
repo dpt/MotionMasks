71d3f0466f2a816cb6ca582d2ecba86b370813b0
Dave Thomas <dave.thomas@metaforic.com>
2012-12-05 16:07:37 +0000

    Build the spans from macros.
    * The critical inner pixel blend is now inlined.
    * Alpha adjustment now happens only once for constant alpha blends.
    
    I tried to reduce the source even further but hit walls with the C preprocessor token pasting.
    
    The functions declared in all8888-generic.c are not 8888-specific so can be refactored again later for use with other formats.

--- libraries/plot/span/rgbx8888.c
+++ libraries/plot/span/rgbx8888.c
@@ -15,87 +15,15 @@
 #define BLUE_SHIFT  16
 #define X_SHIFT     24
 
-#define RED_MASK   (0xFFu << RED_SHIFT)
-#define GREEN_MASK (0xFFu << GREEN_SHIFT)
-#define BLUE_MASK  (0xFFu << BLUE_SHIFT)
-#define X_MASK     (0xFFu << X_SHIFT)
+#include "all8888-generic.c"
 
-// splats any alpha (but this is an 'X' format so we don't care)
-// critical - optimise me once proved working
-static pixelfmt_rgbx8888_t blend_pix_rgbx8888(pixelfmt_rgbx8888_t src1,
-                                              pixelfmt_rgbx8888_t src2,
-                                              int                 alpha)
-{
-  pixelfmt_rgbx8888_t r1, g1, b1;
-  pixelfmt_rgbx8888_t r2, g2, b2;
-
-  if (alpha == 0)
-    return src1;
-  if (alpha == 255)
-    return src2;
-
-  alpha += alpha >= 128;
-
-  r1 = (src1 & RED_MASK) >> RED_SHIFT;
-  r2 = (src2 & RED_MASK) >> RED_SHIFT;
-  r1 = (r1 * (256 - alpha) + r2 * alpha) >> 8;
-
-  g1 = (src1 & GREEN_MASK) >> GREEN_SHIFT;
-  g2 = (src2 & GREEN_MASK) >> GREEN_SHIFT;
-  g1 = (g1 * (256 - alpha) + g2 * alpha) >> 8;
-
-  b1 = (src1 & BLUE_MASK) >> BLUE_SHIFT;
-  b2 = (src2 & BLUE_MASK) >> BLUE_SHIFT;
-  b1 = (b1 * (256 - alpha) + b2 * alpha) >> 8;
-
-  return (r1 << RED_SHIFT) | (g1 << GREEN_SHIFT) | (b1 << BLUE_SHIFT) | X_MASK;
-}
-
-static void span_rgbx8888_blendconst(void       *vdst,
-                                     const void *vsrc1,
-                                     const void *vsrc2,
-                                     int         length,
-                                     int         alpha)
-{
-  pixelfmt_rgbx8888_t       *dst  = vdst;
-  const pixelfmt_rgbx8888_t *src1 = vsrc1;
-  const pixelfmt_rgbx8888_t *src2 = vsrc2;
-
-  while (length--)
-  {
-    *dst = blend_pix_rgbx8888(*src1, *src2, alpha);
-
-    dst++;
-    src1++;
-    src2++;
-  }
-}
-
-static void span_rgbx8888_blendarray(void            *vdst,
-                                     const void      *vsrc1,
-                                     const void      *vsrc2,
-                                     int              length,
-                                     const mmalpha_t *alphas)
-{
-  pixelfmt_rgbx8888_t       *dst  = vdst;
-  const pixelfmt_rgbx8888_t *src1 = vsrc1;
-  const pixelfmt_rgbx8888_t *src2 = vsrc2;
-
-  while (length--)
-  {
-    *dst = blend_pix_rgbx8888(*src1, *src2, *alphas);
-
-    dst++;
-    src1++;
-    src2++;
-    alphas++;
-  }
-}
+SPAN_ALL8888_BLEND_CONST(span_rgbx8888_blendconst, pixelfmt_rgbx8888_t)
+SPAN_ALL8888_BLEND_ARRAY(span_rgbx8888_blendarray, pixelfmt_rgbx8888_t)
 
 const span_t span_rgbx8888 =
 {
-  pixelfmt_rgbx8888,
-  span_all8888_copy,
-  span_rgbx8888_blendconst,
-  span_rgbx8888_blendarray,
+    pixelfmt_rgbx8888,
+    span_all8888_copy,
+    span_rgbx8888_blendconst,
+    span_rgbx8888_blendarray,
 };
